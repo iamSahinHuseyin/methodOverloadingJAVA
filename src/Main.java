fb51e22b660f6983b9e8c11bb021f873b5bcf516
sahin <sahinhuseyinn0307@gmal.com>
2024-05-25 18:09:01 +0300

    Finished Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void skorhesapla(String isim, int puan){
+        System.out.println(isim + "isimli oyuncunun puanı: " +puan );
+    }
+    public static void skorhesapla(int puan){
+        System.out.println("İsimsiz oyuncunun puanı: " +puan );
+    }
+    public static void skorhesapla(String isim){
+        System.out.println(isim + " isimli oyuncunun puanı yoktur.");
+    }
     public static void main(String[] args) {
-
+    //method overloading(method isimleri aynı paramerelerin sayısı veya değişken tipi farklı.)
+       skorhesapla("Hüseyin",95);
+       skorhesapla(95);
+       skorhesapla("Hüseyin");
     }
 }
